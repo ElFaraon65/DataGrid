4889fd7d1e349c1b00ef181cd23f4b616e182375
ElFaraon65 <108295477+ElFaraon65@users.noreply.github.com>
2026-04-18 22:43:24 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Toda la app funciona sin conexión una vez instalada.
 ═══════════════════════════════════════════════════════ */
 
-const CACHE_NAME  = 'datagrid-v1';
+const CACHE_NAME  = 'datagrid-v2';
 const STATIC_ASSETS = [
   'index.html',
   'manifest.json',
