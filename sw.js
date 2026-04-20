67c0846dc52e296df52b96d76614e85a07f1cafe
ElFaraon65 <108295477+ElFaraon65@users.noreply.github.com>
2026-04-20 16:19:23 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Toda la app funciona sin conexión una vez instalada.
 ═══════════════════════════════════════════════════════ */
 
-const CACHE_NAME  = 'datagrid-v4';
+const CACHE_NAME  = 'datagrid-v5';
 const STATIC_ASSETS = [
   'index.html',
   'manifest.json',
